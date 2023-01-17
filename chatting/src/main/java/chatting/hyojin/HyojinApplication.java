0894754c394dc9fb849cef378c0effff1b8d70c1
hyojin <hyoreal51@gmail.com>
2023-01-17 16:27:24 +0900

    basic websocket chatting server

---
+++ chatting/src/main/java/chatting/hyojin/HyojinApplication.java
@@ -0,0 +1,13 @@
+package chatting.hyojin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyojinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyojinApplication.class, args);
+	}
+
+}
---
+++ chatting/src/test/java/chatting/hyojin/HyojinApplicationTests.java
@@ -0,0 +1,13 @@
+package chatting.hyojin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyojinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
